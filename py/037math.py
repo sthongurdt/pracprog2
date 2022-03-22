1f072963d09c3dd59644776294bf5d4a6d99c92d
sthongurdt <sthongurdt@hotmail.com>
2022-03-22 08:54:05 -0500

    agregado manejo de modulo math

---
+++ py/037math.py
@@ -0,0 +1,30 @@
+# python posee un extenso modulo de matematicas
+
+# la funcion min() y max() mostraran los valoeres minimos y maximos 
+x = min(5, 4, 3)
+y = max(5, 6, 7)
+print('min = ', x, ' y max = ', y)
+
+# la funcion abs() devuelve el valor absoluto
+x = abs(-8.765)
+print(x)
+
+# la funcion pow() retorna la potencia
+y = pow(2,3)
+print(y)
+
+# el modulo math extiende la cantidad de funciones que podemos usar
+import math
+
+# la funcion math.sqrt() retorna la raiz cuadrada
+z = math.sqrt(1024)
+print(z)
+
+# math.ceil() retorna el entero mayor y math.floor() el entero menor
+x = math.ceil(5.5)
+y = math.floor(5.5)
+print(x)
+print(y)
+
+# math.pi retorna el valor del numero pi
+print(math.pi)
