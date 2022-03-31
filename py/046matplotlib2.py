86da313ef34a947430c7ce186e1eebba2b2a0f32
sthongurdt <sthongurdt@hotmail.com>
2022-03-30 19:47:38 -0500

    agregado matplotlib3 y modificado matplotlib2

--- py/046matplotlib2.py
+++ py/046matplotlib2.py
@@ -42,11 +42,79 @@ plt.plot(y1)
 plt.plot(y2)
 plt.show()
 
-
 x1 = np.array([0, 1, 4, 3])
 y1 = np.array([3, 8, 1, 10])
 x2 = np.array([0, 1, 2, 4])
 y2 = np.array([6, 2, 7, 11])
 
 plt.plot(x1, y1, x2, y2)
+plt.show()
+
+# se puede agragar etiquetas a los plots, con xlabel para el eje x, con ylabel para el eje y
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Pulso Promedio")
+plt.ylabel("Calorias Quemadas")
+
+plt.show()
+
+# si desea agregar un titulo use title()
+plt.plot(x, y)
+
+plt.title("Datos Smartwatch")
+plt.xlabel("Pulso Promedio")
+plt.ylabel("Calorias Quemadas")
+
+plt.show()
+
+# se puede usar fontdict para pasar los parametros xlabel(), ylabel() y title()
+d1 = {'family':'serif','color':'b','size':20}
+d2 = {'family':'serif','color':'r','size':10}
+
+plt.title("Datos Smartwatch", fontdict = d1)
+plt.xlabel("Pulso Promedio", fontdict = d2)
+plt.ylabel("Calorias Quemadas", fontdict = d2)
+
+plt.plot(x, y)
+plt.show()
+
+# el titulo puede colocarse en lugares (por defecto esta en el centro) como:
+#   left    izquierda
+#   right   derecha
+#   center  centro
+plt.title("Datos Smartwatch", fontdict = d2, loc = 'left')
+plt.xlabel("Pulso Promedio", fontdict = d2)
+plt.ylabel("Calorias Quemadas", fontdict = d2)
+
+plt.plot(x, y)
+plt.show()
+
+# con la funcion grid() puede agregarse una maya a el grafico
+plt.title("Datos Smartwatch", fontdict = d1)
+plt.xlabel("Pulso Promedio", fontdict = d2)
+plt.ylabel("Calorias Quemadas", fontdict = d2)
+
+plt.plot(x, y)
+plt.grid()
+plt.show()
+
+# la maya puede colocarse en diferentes ejes
+plt.title("Datos Smartwatch", fontdict = d1)
+plt.xlabel("Pulso Promedio", fontdict = d2)
+plt.ylabel("Calorias Quemadas", fontdict = d2)
+
+plt.plot(x, y)
+plt.grid(axis = 'x')
+plt.show()
+
+plt.plot(x, y)
+plt.grid(axis = 'y')
+plt.show()
+
+# la maya puede tener color (color), estilo (linestyle) y grosor (linewidth)
+plt.plot(x, y)
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
 plt.show()
---
+++ py/047matplotlib3.py
@@ -0,0 +1,115 @@
+# la funcion subplot() puede graficar multiples plots en una figura
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+
+plt.show()
+
+# la funcion subplot() toma 3 argumentos, el primer argumento es la fila, el segundo argumento es la columna, el tercer argumento es el indice.
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 1, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 1, 2)
+plt.plot(x,y)
+
+plt.show()
+
+# ------------------------------------------------------------------------------------
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 1)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 2)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 3)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 4)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 5)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 6)
+plt.plot(x,y)
+
+plt.show() 
+
+#---------------------------------------------------------------------------
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+plt.title("VENTAS")
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+plt.title("INGRESOS")
+
+plt.show()
+
+# la funcion suptitle() agrega un titulo para toda la figura
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+plt.title("VENTAS")
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+plt.title("INGRESOS")
+
+plt.suptitle("TIENDA ")
+plt.show()
